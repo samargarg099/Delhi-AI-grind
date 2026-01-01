e9ac22e7cbb26c5d2c4fecd27482f1182977e473
samargarg099 <samargarg.099@gmail.com>
2026-01-01 21:53:12 +0630

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,32 @@
+import sqlite3
+
+def create_db():
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username TEXT PRIMARY KEY,
+            password TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
+def add_user(username, password):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO users VALUES (?, ?)", (username, password))
+    conn.commit()
+    conn.close()
+
+def login_user(username, password):
+    conn = sqlite3.connect("users.db")
+    c = conn.cursor()
+    c.execute(
+        "SELECT * FROM users WHERE username=? AND password=?",
+        (username, password)
+    )
+    data = c.fetchone()
+    conn.close()
+    return data
 
