7b03a31789311f5bd50899c5716790b2bda8646c
samargarg099 <samargarg.099@gmail.com>
2026-01-01 22:10:20 +0630

    Update database.py

--- database.py
+++ database.py
@@ -33,23 +33,6 @@ def login_user(username, password):
 def save_current_chapter(username, subject, chapter):
     conn = sqlite3.connect("users.db")
     c = conn.cursor()
-    c.execute("""
-        CREATE TABLE IF NOT EXISTS progress (
-            username TEXT,
-            subject TEXT,
-            chapter TEXT
-        )
-    """)
-    c.execute(
-        "INSERT INTO progress VALUES (?, ?, ?)",
-        (username, subject, chapter)
-    )
-    conn.commit()
-    conn.close()
-
-def save_current_chapter(username, subject, chapter):
-    conn = sqlite3.connect("users.db")
-    c = conn.cursor()
 
     c.execute("""
         CREATE TABLE IF NOT EXISTS progress (
